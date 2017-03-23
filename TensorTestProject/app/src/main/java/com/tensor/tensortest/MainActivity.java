46ac4989844d501ba5f70207c4aade68db24b65c
Alexander Efimov <teeeses@mail.ru>
2017-03-23 12:54:28 +0300

    first commit

---
+++ TensorTestProject/app/src/main/java/com/tensor/tensortest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tensor.tensortest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tensor.tensortest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
