38cfffb5638110bb8cd2d5c1e22949a46700aefa
Alexander Efimov <teeeses@mail.ru>
2017-03-24 15:31:09 +0300

    Реализован адаптер для списка новостей, без дизайна

---
+++ TensorTestProject/app/src/main/java/com/tensor/tensortest/adapters/NewsAdapter.java
@@ -0,0 +1,64 @@
+package com.tensor.tensortest.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.tensor.tensortest.R;
+
+import java.util.List;
+
+/**
+ * Created by develop on 24.03.2017.
+ */
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
+
+    private List<String> links;
+
+    public NewsAdapter(List<String> records) {
+        this.links = records;
+    }
+
+    /**
+     * Создание новых View и ViewHolder элемента списка, которые впоследствии могут переиспользоваться.
+     */
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_news_item, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    /**
+     * Заполнение виджетов View данными из элемента списка с номером i
+     */
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        String title = links.get(i);
+        viewHolder.tvNewTitle.setText(title);
+    }
+
+    @Override
+    public int getItemCount() {
+        return links.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    /**
+     * Реализация класса ViewHolder, хранящего ссылки на виджеты.
+     */
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvNewTitle;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvNewTitle = (TextView) itemView.findViewById(R.id.tvNewTitle);
+        }
+    }
+}
--- TensorTestProject/app/src/main/java/com/tensor/tensortest/fragments/NewsFragment.java
+++ TensorTestProject/app/src/main/java/com/tensor/tensortest/fragments/NewsFragment.java
@@ -2,15 +2,18 @@ package com.tensor.tensortest.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.tensor.tensortest.MainActivity;
 import com.tensor.tensortest.R;
-import com.tensor.tensortest.Utils.Settings;
 import com.tensor.tensortest.Web.SiteXmlParsing;
 import com.tensor.tensortest.Web.WebSetting;
+import com.tensor.tensortest.adapters.NewsAdapter;
 
 import java.util.List;
 
@@ -26,23 +29,30 @@ import rx.schedulers.Schedulers;
 
 public class NewsFragment extends Fragment {
 
+    private RecyclerView newsRecyclerView;
+    private NewsAdapter adapter;
+
+    private SwipeRefreshLayout swipeRefresher;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_news, container, false);
 
-        queryURLs(WebSetting.SITE_URL)
-                .subscribe(new Action1<List<String>>() {
-                    @Override
-                    public void call(List<String> links) {
-                        Observable.from(links)
-                            .subscribe(new Action1<String>() {
-                                @Override
-                                public void call(String link) {
-                                    Log.d(Settings.TAG, "Link: " + link);
-                                }
-                            });
-                    }
-                });
+        newsRecyclerView = (RecyclerView) view.findViewById(R.id.newsRecyclerView);
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity());
+        newsRecyclerView.setLayoutManager(manager);
+
+
+        swipeRefresher = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        swipeRefresher.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshNewsList();
+                swipeRefresher.setRefreshing(true);
+            }
+        });
+
+        refreshNewsList();
 
         return view;
     }
@@ -50,20 +60,31 @@ public class NewsFragment extends Fragment {
     /**
      * Обновление новостного листа
      */
-    public void updateNewsList() {
+    public void refreshNewsList() {
+        ((MainActivity)getActivity()).setToolbarTitle(R.string.loading);
+        queryURLs(WebSetting.SITE_URL)
+            .subscribe(new Action1<List<String>>() {
+                @Override
+                public void call(List<String> links) {
+                    adapter = new NewsAdapter(links);
+                    newsRecyclerView.setAdapter(adapter);
 
+                    swipeRefresher.setRefreshing(false);
+                    ((MainActivity)getActivity()).setToolbarTitle(R.string.news);
+                }
+            });
     }
 
     private Observable<List<String>> queryURLs(String link) {
         SiteXmlParsing webParsing = new SiteXmlParsing();
-        return Observable.create(
-                new Observable.OnSubscribe<List<String>>() {
-                    @Override
-                    public void call(Subscriber<? super List<String>> subscriber) {
-                        subscriber.onNext(webParsing.getURLs(link));
-                        subscriber.onCompleted();
-                    }
-                }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+
+        return Observable.create(new Observable.OnSubscribe<List<String>>() {
+            @Override
+            public void call(Subscriber<? super List<String>> subscriber) {
+                subscriber.onNext(webParsing.getURLs(link));
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
     }
 
 }
